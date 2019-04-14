691d8f0c6cdbf4f774cb05399b68dfdde13b8a73
David Poslt <david.poslt@gmail.com>
2019-04-14 20:06:36 +0200

    supermario block

---
+++ mario.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+
+## super mario - ? ##
+
+print('hlavoubusil')
+for i in range(3):
+    print('?',end="")
+
+print()
+
+
+
+
+## super marion - wall ##
+
+print('wall')
+for i in range(3):
+    print('#')
+
+
+## super mario - squar ##
+
+print('block')
+for i in range(3):
+    print('#',end="")
+    for i in range(3):
+        print('#',end="")
+
+    print()
+
+
