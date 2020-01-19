a59c8235401da81a2a4ba74d1672f4ae08277ad7
David Poslt <david.poslt@gmail.com>
2020-01-19 20:06:59 +0100

    swith celsium and fahrheit

--- temp.py
+++ temp.py
@@ -2,11 +2,22 @@ import sys
 
 f = sys.argv
 
-if (n := len(f)) == 1:
+if (n := len(f)) < 2: #  arguments == programme name (temp.py)
     print(f'Zadejte prosim vice nez {n} argument')
     sys.exit()
-else:
-    for pocet in f:
-        if pocet.isdigit():
-            c = (int(pocet)-32)*5/9
-            print(f'{pocet}°Fahrnheita je {c:.2f}° celsia')
+elif len(f) > 3:
+    print('Priliz mnoho argumentu')
+    sys.exit()
+
+if f[2] == 'f' or f[2] == 'F':     #from fahrnheit to celsia
+    if (pocet := f[1]).isdigit():
+        c = (int(pocet)-32)*5/9
+        print(f'{pocet}° Fahrnheita je {c:.2f}° celsia')
+
+elif f[2] == 'c' or f[2] == 'C':   #from celsia to fahrnheit
+    if (pocet := f[1]).isdigit():
+        c = (9*int(pocet))/5 + 32
+        print(f'{pocet}° celsia  je {c:.2f}° Fahrnheita')
+
+elif f[2] != 'f' or f[2] != 'c':
+    print('Wrong argument. You can use "f" or "c" only')
